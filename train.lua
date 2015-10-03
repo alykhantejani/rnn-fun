4c60d50503b37860b9d9af149b1af3e6611d8b68
Alykhan Tejani <alykhan.tejani@gmail.com>
2015-10-03 15:27:35 +0100

    got basic trianing working, need to add learning rate decay etc.

--- train.lua
+++ train.lua
@@ -1,56 +1,75 @@
+require 'torch'
 require 'nngraph'
-local minibatch_loader = require 'util.CharSplitLMMinibatchLoader'
-require 'util.OneHot'
+require 'utils.OneHot'
 require 'rnn_model'
 require 'optim'
+local minibatch_loader = require 'utils.CharSplitLMMinibatchLoader'
+local model_utils = require 'utils.model_utils'
 
+-------------------------------------------------------------------
+----------------------Command Line Params--------------------------
+-------------------------------------------------------------------
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text('Arguments')
+cmd:argument('-data_dir','txt file with data')
+cmd:text('Options')
+cmd:option('-grad_clip',5,'clip gradients at this value')
+cmd:option('-train_frac',0.95,'fraction of data that goes into train set')
+cmd:option('-val_frac',0.05,'fraction of data that goes into validation set')
+cmd:option('-batch_size', 64, 'batch size')
+cmd:option('-max_epoch', 10, 'max epochs to train for')
+cmd:option('-sequence_length', 10, 'sequence length to train on')
+cmd:option('-num_hidden_units', 128, 'number of hidden units')
+cmd:option('-learning_rate',2e-3,'learning rate')
+cmd:option('-learning_rate_decay',0.97,'learning rate decay')
+cmd:option('-learning_rate_decay_after',10,'in number of epochs, when to start decaying the learning rate')
+cmd:option('-decay_rate',0.95,'decay rate for rmsprop')
+cmd:option('-weights', '', 'pretrained model to begin training from')
+cmd:option('-display_iteration', 20, 'when to display training accuracy (1 iteration = 1 batch)')
+cmd:option('-test_iteration', 20, 'when to display validation accuracy (1 iteration = 1 batch')
+cmd:option('-optim_state','','path to the optimiser state file')
+cmd:option('-snapshot_dir', './snapshots/', 'snapshot directory')
+cmd:option('-snapshot_iteration', 1, 'snapshot after how many epochs?')
+cmd:option('-gpu_id', -1, 'gpu ID to train on')
+cmd:option('-log', '', 'output log file')
 
---we need to clone the rnn many times as
---each one has it's own output and gradInput
---variables (and are not overwritten by next time step)
-function clone_many_times(model, times)
-	local clones = {}
-	
-	local param, grad_param = model:getParameters()
-	local p, grad_p = model:parameters()
+local params = cmd:parse(arg)
 
-	for t = 1, times do
-		local clone = model:clone()
-		local clone_p, clone_grad_p = clone:parameters()
-		--set all clones to the same parameter view
-		for i = 1, #p do
-			clone_p[i]:set(p[i])
-			clone_grad_p[i]:set(grad_p[i])
-		end
-		---------------------------------------------
-		table.insert(clones, clone)
-	end
-	collectgarbage()
-	return clones;
+if params.log ~= '' then
+	cmd:log(params.log, params)
 end
 
+-------------------------------------------------------------------
+----------------------Initialize Variables-------------------------
+-------------------------------------------------------------------
 
+local seq_length = params.sequence_length
+local batch_size = params.batch_size
+local max_epochs = params.max_epoch
+local clip = params.grad_clip
+local num_hidden_units = params.num_hidden_units
+local display_iteration = params.display_iteration
+local test_frac = math.max(0, 1 - (params.train_frac + params.val_frac))
+local split_sizes = {params.train_frac, params.val_frac, test_frac} 
 
-local seq_length = 10
-local batch_size = 5
-local loader = minibatch_loader.create('data/tinyshakespeare', batch_size, seq_length, {0.95, 0.05, 0})
+local loader = minibatch_loader.create(params.data_dir, batch_size, seq_length, split_sizes)
 local vocab_size = loader.vocab_size
 local vocab = loader.vocab
 
-print('vocab size: '..vocab_size)
+print('loaded dataset with ' .. vocab_size .. ' unique characters')
 
 --input_vocab_size, rnn_hidden_size, output_vocab_size
-model = create_rnn(vocab_size, 128, vocab_size)
-criterion = nn.ClassNLLCriterion()
+local model = create_rnn(vocab_size, num_hidden_units, vocab_size)
+local criterion = nn.ClassNLLCriterion()
 
-
-cloned_models = clone_many_times(model, seq_length)
-cloned_criteria = clone_many_times(criterion, seq_length)
+local cloned_models = model_utils.clone_many_times(model, seq_length)
+local cloned_criteria = model_utils.clone_many_times(criterion, seq_length)
 
 -- put the above things into one flattened parameters tensor
-params, grad_params = model_utils.combine_all_parameters(cloned_models)
+params, grad_params = model_utils.combine_all_parameters(unpack(cloned_models))
 
-local h_init = torch.zeros(batch_size, 128)
+local h_init = torch.zeros(batch_size, num_hidden_units)
 
 function feval(x)
 	if x ~= params then
@@ -62,7 +81,7 @@ function feval(x)
 	local input, target = loader:next_batch(1)
 
 	local hidden_states = {}
-	hidden_states[0] = torch.zeros(batch_size, 128)
+	hidden_states[0] = torch.zeros(batch_size, num_hidden_units)
 	
 	local predictions = {}
 	local loss = 0
@@ -81,7 +100,7 @@ function feval(x)
 	-------------Backward Pass-----------------
 	--make error at time t 0 as there is no influence from the future
 	local dhidden = {}
-	dhidden[seq_length] = torch.zeros(batch_size, 128) -- no error from the futuer hidden node
+	dhidden[seq_length] = torch.zeros(batch_size, num_hidden_units) -- no error from the futuer hidden node
 
 	for t = seq_length,1,-1 do
 		local dcrit = cloned_criteria[t]:backward(predictions[t], target[{{}, t}])
@@ -92,11 +111,25 @@ function feval(x)
 
 	h_init = hidden_states[#hidden_states] -- update next batch initial state
 	grad_params:div(seq_length)
-	grad_params:clamp(-5, 5) -- clamp to avoid exploding gradients
+	grad_params:clamp(-clip, clip) -- clamp to avoid exploding gradients
 
 	return loss, grad_params
 end
 
-config = {learningRate = 0.01, momentum = 0.9}
---This function updates the global parameters variable (which is a view on the models parameters)
-optim.sgd(func_eval, parameters, config)
+-------------------------------------------------------------------------
+---------------------------Main loop-------------------------------------
+-------------------------------------------------------------------------
+local optim_state = {learningRate = params.learning_rate, alpha = params.decay_rate}
+local timer = torch.Timer()
+
+for epoch = 1, max_epochs do
+	print(string.format('Starting Epoch [%d]', epoch))
+	for iter = 1, loader.ntrain do
+		timer:reset()
+		p, cost = optim.rmsprop(feval, params, optim_state)
+		loss = cost[1]
+		if (iter % display_iteration) == 0 then
+			print(string.format('Epoch [%d] Iteration [%d]: Loss = %.4f (this batch took %.4f ms)', epoch, iter, loss, (timer:time().real * 1000.0)))
+		end
+	end
+end
---
+++ utils/model_utils.lua
@@ -0,0 +1,144 @@
+require 'torch'
+require 'nn'
+
+local model_utils = {}
+
+--we need to clone the rnn many times as
+--each one has it's own output and gradInput
+--variables (and are not overwritten by next time step)
+function model_utils.clone_many_times(model, times)
+    local clones = {}
+    local param, grad_param
+    local p, grad_p
+
+    if model.parameters then
+        param, grad_param = model:getParameters()
+        p, grad_p = model:parameters()
+    end
+
+    for t = 1, times do
+        local clone = model:clone()
+
+        if model.parameters then
+            local clone_p, clone_grad_p = clone:parameters()
+            --set all clones to the same parameter view
+            for i = 1, #p do
+                clone_p[i]:set(p[i])
+                clone_grad_p[i]:set(grad_p[i])
+            end
+            ---------------------------------------------
+        end
+        table.insert(clones, clone)
+    end
+    collectgarbage()
+    return clones;
+end
+
+--Flattens all parameters in a list of models
+--So for example, in a list of cloned models
+--it will flatten all parmeters of all models into
+--one tensor. If these models have shared weights
+--it will make them point to the same location in this
+--tensor
+function model_utils.combine_all_parameters(...)
+    --[[ like module:getParameters, but operates on many modules ]]--
+
+    -- get parameters
+    local networks = {...}
+    local parameters = {}
+    local gradParameters = {}
+    for i = 1, #networks do
+        local net_params, net_grads = networks[i]:parameters()
+
+        if net_params then
+            for _, p in pairs(net_params) do
+                parameters[#parameters + 1] = p
+            end
+            for _, g in pairs(net_grads) do
+                gradParameters[#gradParameters + 1] = g
+            end
+        end
+    end
+
+    local function storageInSet(set, storage)
+        local storageAndOffset = set[torch.pointer(storage)]
+        if storageAndOffset == nil then
+            return nil
+        end
+        local _, offset = unpack(storageAndOffset)
+        return offset
+    end
+
+    -- this function flattens arbitrary lists of parameters,
+    -- even complex shared ones
+    local function flatten(parameters)
+        if not parameters or #parameters == 0 then
+            return torch.Tensor()
+        end
+        local Tensor = parameters[1].new
+
+        local storages = {}
+        local nParameters = 0
+        for k = 1,#parameters do
+            local storage = parameters[k]:storage()
+            if not storageInSet(storages, storage) then
+                storages[torch.pointer(storage)] = {storage, nParameters}
+                nParameters = nParameters + storage:size()
+            end
+        end
+
+        local flatParameters = Tensor(nParameters):fill(1)
+        local flatStorage = flatParameters:storage()
+
+        for k = 1,#parameters do
+            local storageOffset = storageInSet(storages, parameters[k]:storage())
+            parameters[k]:set(flatStorage,
+                storageOffset + parameters[k]:storageOffset(),
+                parameters[k]:size(),
+                parameters[k]:stride())
+            parameters[k]:zero()
+        end
+
+        local maskParameters=  flatParameters:float():clone()
+        local cumSumOfHoles = flatParameters:float():cumsum(1)
+        local nUsedParameters = nParameters - cumSumOfHoles[#cumSumOfHoles]
+        local flatUsedParameters = Tensor(nUsedParameters)
+        local flatUsedStorage = flatUsedParameters:storage()
+
+        for k = 1,#parameters do
+            local offset = cumSumOfHoles[parameters[k]:storageOffset()]
+            parameters[k]:set(flatUsedStorage,
+                parameters[k]:storageOffset() - offset,
+                parameters[k]:size(),
+                parameters[k]:stride())
+        end
+
+        for _, storageAndOffset in pairs(storages) do
+            local k, v = unpack(storageAndOffset)
+            flatParameters[{{v+1,v+k:size()}}]:copy(Tensor():set(k))
+        end
+
+        if cumSumOfHoles:sum() == 0 then
+            flatUsedParameters:copy(flatParameters)
+        else
+            local counter = 0
+            for k = 1,flatParameters:nElement() do
+                if maskParameters[k] == 0 then
+                    counter = counter + 1
+                    flatUsedParameters[counter] = flatParameters[counter+cumSumOfHoles[k]]
+                end
+            end
+            assert (counter == nUsedParameters)
+        end
+        return flatUsedParameters
+    end
+
+    -- flatten parameters and gradients
+    local flatParameters = flatten(parameters)
+    local flatGradParameters = flatten(gradParameters)
+
+    -- return new flat vector that contains all discrete parameters
+    return flatParameters, flatGradParameters
+end
+
+return model_utils
